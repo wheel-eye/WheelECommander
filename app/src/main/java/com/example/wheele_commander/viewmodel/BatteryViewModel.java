fa6d297a7be8f1a3cf307898f3531438483429cb
konradpawlikowski <s2022345@ed.ac.uk>
2023-02-10 19:20:23 +0000

    Included documentation for MessageType and BatteryViewModel.

--- app/src/main/java/com/example/wheele_commander/viewmodel/BatteryViewModel.java
+++ app/src/main/java/com/example/wheele_commander/viewmodel/BatteryViewModel.java
@@ -4,19 +4,44 @@ import static com.example.wheele_commander.viewmodel.MessageType.BATTERY_UPDATE;
 
 import android.content.ComponentName;
 import android.content.ServiceConnection;
+
 import android.os.IBinder;
 import android.os.Message;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.wheele_commander.backend.INetworkClient;
 import com.example.wheele_commander.backend.NetworkClient;
 
+import org.jetbrains.annotations.NotNull;
+
+import kotlin.jvm.Throws;
+
+/**
+ * stores information on the battery of the connected hardware and handles {@code BATTERY_UPDATE} messages.
+ *
+ * @author Konrad Pawlikowski
+ * @author Peter Marks
+ * @see com.example.wheele_commander.viewmodel.IMessageSubscriber
+ * @see androidx.lifecycle.ViewModel
+ * @version 1.0
+ * @since 08/02/2023
+ */
 public class BatteryViewModel extends ViewModel implements IMessageSubscriber {
-    public static final int MAXIMUM_MILEAGE = 12000; // represents 12km
-    public final MutableLiveData<Integer> batteryCharge;
-    public final MutableLiveData<Integer> estimatedMileage;
+    /**
+     * recalls the mean maximum mileage of the device on smooth terrain with no incline, as measured during testing.
+     *
+     * each unit represents a
+     * <a href="https://physics.nist.gov/cuu/Units/prefixes.html">decimeter</a>
+     * so that a view may display results with 1 decimal point of precision.
+     *
+     * @since 1.0
+     */
+    public static final int MAXIMUM_MILEAGE = 120000;
+    private final MutableLiveData<Integer> batteryCharge;
+    private final MutableLiveData<Integer> estimatedMileage;
     private INetworkClient networkClient;
 
     public BatteryViewModel() {
@@ -47,16 +72,47 @@ public class BatteryViewModel extends ViewModel implements IMessageSubscriber {
         return serviceConnection;
     }
 
-    public MutableLiveData<Integer> getBatteryCharge() {
+    /**
+     * returns the current charge of the connected hardware.
+     *
+     * @return Returns the charge of the hardware battery where each unit represents 0.1%
+     * of battery charge.
+     *
+     * @see LiveData
+     * @since 1.0
+     */
+    public LiveData<Integer> getBatteryCharge() {
         return batteryCharge;
     }
 
-    public MutableLiveData<Integer> getEstimatedMileage() {
+    /**
+     * returns the estimated mileage of the connected hardware.
+     *
+     * @return Returns the estimated mileage of the hardware where each unit represents
+     * a <a href="https://physics.nist.gov/cuu/Units/prefixes.html">decimeter</a>.
+     *
+     * @see LiveData
+     * @since 1.0
+     */
+    public LiveData<Integer> getEstimatedMileage() {
         return estimatedMileage;
     }
 
+    /**
+     * Takes a {@code BATTERY_UPDATE} message and updates the current {@code batteryCharge} and
+     * {@code estimatedMileage}.
+     *
+     * @param msg refer to {@link MessageType}.{@code BATTERY_UPDATE} for clear specifications
+     * on the message structure.
+     *
+     * @exception  IllegalArgumentException Thrown when given a message that isn't a
+     * {@code BATTERY_UPDATE}.
+     * @exception IndexOutOfBoundsException Thrown when given an unknown message nominal.
+     *
+     * @since 1.0
+     */
     @Override
-    public void handleMessage(Message msg) {
+    public void handleMessage(@NotNull Message msg) {
         if (msg.what == BATTERY_UPDATE.ordinal()) {
             batteryCharge.setValue(msg.arg1);
             estimatedMileage.setValue(MAXIMUM_MILEAGE / batteryCharge.getValue() * 1000);
--- app/src/main/java/com/example/wheele_commander/viewmodel/MessageType.java
+++ app/src/main/java/com/example/wheele_commander/viewmodel/MessageType.java
@@ -1,21 +1,50 @@
 package com.example.wheele_commander.viewmodel;
 
+/**
+ * Type of supported nominal for communication with ViewModels which implement {@link IMessageSubscriber}.
+ *
+ * Usage example:
+ * <pre>{@code msg.what = MessageType.BATTERY_UPDATE.ordinal();}</pre>
+ *
+ * @author Konrad Pawlikowski
+ * @version 1.0
+ * @since 06/02/2023
+ */
 public enum MessageType {
-    /*
-    JOYSTICK_MOVEMENT:
-    arg1 = angle
-    arg2 = power
+    /**
+     * nominal to indicate a message contains data on joystick inputs.
+     *
+     * the corresponding message should have
+     * <pre>{@code
+     * msg.arg1 = joystick_angle;
+     * msg.arg2 = joystick_power;
+     * }</pre>
+     *
+     * @since 1.0
      */
     JOYSTICK_MOVEMENT,
-    /*
-    BATTERY_UPDATE:
-    arg1 = charge
+    /**
+     * nominal to indicate a message contains data on the battery of the connected hardware.
+     *
+     * the corresponding message should have
+     * <pre>{@code
+     * msg.arg1 = battery_charge;
+     * }</pre>
+     *
+     * @since 1.0
      */
     BATTERY_UPDATE,
-    /*
-    VELOCITY_UPDATE:
-    arg1 = velocity
+    /**
+     * nominal to indicate a message contains data on the velocity of the connected hardware.
+     *
+     * the corresponding message should have
+     * <pre>{@code
+     * msg.arg1 = forward_velocity;
+     * }</pre>
+     *
+     * @since 1.0
      */
     VELOCITY_UPDATE,
+    // WARNING_MESSAGE should be replaced with a separate WarningType.
     WARNING_MESSAGE
 }
