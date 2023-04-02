d17e5cbc341047a98ce64fc6651b3677b938335f
PeepoM <peter.marks@outlook.sk>
2023-04-02 14:08:34 +0100

    changed MAC addr to raspi

--- app/src/main/java/com/example/wheele_commander/backend/bluetooth/BluetoothService.java
+++ app/src/main/java/com/example/wheele_commander/backend/bluetooth/BluetoothService.java
@@ -8,8 +8,8 @@ import android.widget.Toast;
 import com.example.wheele_commander.backend.CommunicationService;
 
 public class BluetoothService extends CommunicationService {
-    //    private static final String MAC_ADDRESS = "DC:A6:32:18:06:59";
-    private static final String MAC_ADDRESS = "90:61:AE:3F:DE:B3";
+        private static final String MAC_ADDRESS = "DC:A6:32:18:06:59";
+//    private static final String MAC_ADDRESS = "90:61:AE:3F:DE:B3";
 
     protected String TAG = "BluetoothService";
 
